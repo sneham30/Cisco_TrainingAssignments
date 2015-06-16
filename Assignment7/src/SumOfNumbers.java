686ebab027c498d1b2bc0224e9528ef13c82c8ac
sneham <sneham@DIN34000330.corp.capgemini.com>
2015-06-16 14:39:15 +0530

    Assignment7

---
+++ Assignment7/src/SumOfNumbers.java
@@ -0,0 +1,13 @@
+
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		int n =20,sum= 0;
+		 for(int i=0;i<=n;i++){
+			 sum = sum + i;
+			 
+		 }
+ System.out.println("Sum of " + n + "numbers :" + sum);
+	}
+
+}
