ff3d65a179f74f60e8c781e36bb5878855ab9a9d
sneham <sneham@DIN34000330.corp.capgemini.com>
2015-06-16 11:41:14 +0530

    changes to add long

--- Assignment1/src/Casting/DownCasting.java
+++ Assignment1/src/Casting/DownCasting.java
@@ -4,12 +4,27 @@ public class DownCasting {
 	public static void main(String args[]){
 		double number = 12312.12119;
 		float fnum = (float) number;
+		long lnum = (long) fnum;
 		int inum = (int) fnum;
 		short snum = (short) inum;
 		byte bnum = (byte) snum;
 		System.out.println("Double number " +number +" casted to float:" +fnum);
-		System.out.println("Float number " +fnum + " casted to int:" +inum);
+		System.out.println("Float number " +fnum + " casted to long:" +lnum);
+		System.out.println("long number " +lnum + " casted to int:" +inum);
 		System.out.println("int number " +inum + " casted to short:" +snum);
 		System.out.println("short number " +snum + " casted to byte:" +bnum);
+		
+		
+		float f = 10.3f;
+		//double l = 10.6;
+		int i =1000;
+		short s = 122;
+		byte b = 12;
+		
+		System.out.println("Float:"+f);
+		System.out.println("int:"+i);
+		System.out.println("short"+s);
+		System.out.println("byte:"+b);
+		
 	}
 }
--- Assignment1/src/Casting/Upcasting.java
+++ Assignment1/src/Casting/Upcasting.java
@@ -6,11 +6,25 @@ public class Upcasting {
 		byte b = 99;
 		short s = (short) b;
 		int i = (int) s;
-		float f = (float) i;
+		long l = (long) i;
+		float f = (float) l;
 		double d = (double) f ;
 		System.out.println("byte number " +b +" casted to short:" +s);
 		System.out.println("short number " +s + " casted to int:" +i);
-		System.out.println("int number " +i + " casted to float:" +f);
+		System.out.println("int number " +i + " casted to long:" +l);
+		System.out.println("long number " +i + " casted to float:" +f);
 		System.out.println("float number " +f + " casted to double:" + d);
+		
+		short s1 =  b;
+		int i1 = s;
+		long l1 = i;
+		float f1 = l1;
+		double d1=  f ;
+		System.out.println("byte:" +b);
+		System.out.println("short: "+s1);
+		System.out.println("int:" +i1);
+		System.out.println("long:"+l1);
+		System.out.println("float:" +f1);
+		System.out.println("double:"+d1);
 	}
 }
